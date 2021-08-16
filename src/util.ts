0d7353b7c163d69247798bcfa070e0284e26e8e6
Jeremy Milledge <jez.milledge@gmail.com>
2021-08-16 21:23:25 +1000

    fix: lint

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,9 @@
-import chalk from "chalk";
-import { Request } from "express";
+import chalk from 'chalk';
+import { Request } from 'express';
 
 export const logger = {
   count: (req: Request, arr: any[]) =>
-  console.log(chalk`{rgb(255,255,255).bgBlue  ${req.route.path} } returning ${arr.length} results to ${req.headers.origin}`)
-    
-}
+    console.log(
+      chalk`{rgb(255,255,255).bgBlue  ${req.route.path} } returning ${arr.length} results to ${req.headers.origin}`
+    ),
+};
